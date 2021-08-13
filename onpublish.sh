b7f82c3f8b05b79a81aeaa64a630dee4986d9783
dv0dv0 <valdezdevin@gmail.com>
2021-08-12 23:16:48 -0700

    Update onpublish.sh

--- onpublish.sh
+++ onpublish.sh
@@ -4,10 +4,12 @@ MYDATE=$(date +%Y-%m-%dT%H:%M:%S%z)
 STREAMKEY=$1
 SOURCE=$2
 STATUS=active
+MYIP=$(hostname -i)
 
 JSON_STRING=$( jo -p \
         streamkey=$(jo S=$STREAMKEY) \
         srce=$(jo S=$SOURCE) \
+        rtmpip=$(jo S=$MYIP) \
         timestamp=$(jo S=$MYDATE) \
         status=$(jo S=$STATUS) \
         )
